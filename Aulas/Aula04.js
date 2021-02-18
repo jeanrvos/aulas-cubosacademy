9001abec6a4c073d3760bd95a34840fa61bd53b1
jeanrvos <velasco.jean@gmail.com>
2021-02-18 22:41:30 +0000

    Aula 04 concluída

---
+++ Aulas/Aula04.js
@@ -0,0 +1,12 @@
+const nome = 'Jean';
+//const não pode ser modificado (constante)
+let idade = 19;
+
+
+console.log(nome);
+console.log(idade);
+
+idade = 20;
+//depois de declarar a variável, não precisa digitar "let" novamente para alterar o valor dela
+
+console.log(idade);
