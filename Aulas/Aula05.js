f994c3a4c97c3ad1c4d43aafb2ff7122b58e5810
jeanrvos <velasco.jean@gmail.com>
2021-02-18 23:09:21 +0000

    Aula 05 concluída

---
+++ Aulas/Aula05.js
@@ -0,0 +1,37 @@
+let resultado = 2 + 2;
+console.log(resultado);
+
+resultado = 2 - 2;
+console.log(resultado);
+
+resultado = 2 * 2;
+console.log(resultado);
+
+resultado = 2 / 2;
+console.log(resultado);
+
+resultado = 3 % 2;
+console.log(resultado);
+
+resultado = 2 + 2 * 3 / 3 - 6;
+console.log(resultado);
+
+resultado = resultado + 2;
+console.log(resultado);
+
+resultado += 2;
+// ou resultado -= *= /=
+console.log(resultado);
+
+resultado -= 2;
+console.log(resultado);
+
+resultado++;
+// ou resultado--;
+console.log(resultado);
+
+resultado += 3;
+console.log(resultado);
+
+resultado = (2 + 2) / (3 * 2);
+console.log(resultado);
