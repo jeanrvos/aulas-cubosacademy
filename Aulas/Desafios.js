e27191f019b4a81ea24570983a8d7d54f8ac18fd
jeanrvos <velasco.jean@gmail.com>
2021-02-20 12:45:25 +0000

    Minhas respostas aos desafios

---
+++ Aulas/Desafios.js
@@ -0,0 +1,174 @@
+// 01 - Divisível por x
+
+// function solucao(numero, x) {
+// 	// seu código aqui
+//     const resto = numero % x;
+    
+//     if (resto === 0) {
+//         console.log(true);
+//     } else {
+//         console.log(false);
+//     }
+// }
+
+
+// 02 - Jackpot
+
+// function solucao(resultado1, resultado2, resultado3) {
+// 	// seu código aqui	
+//     if(resultado1 == resultado2 && resultado2 == resultado3) {
+//         console.log(true);
+//     } else {
+//         console.log(false);
+//     }
+// }
+
+
+// 03 - Derrapagem
+
+// function solucao(velocidade, aceleracao) {
+// 	//seu código aqui  
+//     let resultado = ((velocidade * velocidade) * -1) / (2 * aceleracao);
+//     console.log(resultado);  
+// }
+
+// function processData(input) {
+//     const { velocidade, aceleracao } = JSON.parse(input);
+//   	solucao(velocidade, aceleracao);
+// }
+
+
+// 04 - Colisão
+
+// function solucao(velocidade, aceleracao, distanciaObjeto) {
+//   //seu código aqui
+//     let distanciaDerrapagem = ((velocidade * velocidade) * -1) / (2 * aceleracao)
+    
+//     if (distanciaObjeto < distanciaDerrapagem) {
+//         console.log("COLISAO A FRENTE")        
+//         } else if (distanciaObjeto === distanciaDerrapagem) {
+//                    console.log("NAO ACELERE")
+//                    } else {
+//                        console.log("CAMINHO SEGURO")
+//                    }
+// }
+
+
+// 05 - Enésimo número par
+
+// function solucao(n) {
+//   // seu código aqui
+//     let numeroPar = 2 * n;
+    
+//     console.log(numeroPar);
+    
+// }
+
+
+// 06 - Black friday
+
+// function solucao(produtos, valorMaximo, avaliacaoMinima) {
+// // seu código aqui
+//     let listaPresentesOk = [];
+        
+//     for (let i = 0; i < produtos.length; i++) {
+//         const presente = produtos[i];
+//         if (presente.valor > valorMaximo || presente.avaliacao < avaliacaoMinima) {
+//             continue;            
+//             } else {
+//                 listaPresentesOk.push(presente);
+//             }            
+//     }
+    
+//     console.log(listaPresentesOk);
+  
+// }
+
+
+// 07 - Purificação
+
+// function solucao(stringCorrompida) {
+// 	// seu código aqui
+//     const caracteresEspeciais = "";
+       
+//     let letraNova = "";
+//     let resposta = "";
+    
+//     for (i = 0; i < stringCorrompida.length; i++) {
+//         const letraOriginal = stringCorrompida[i];
+     
+//         if (letraOriginal === "!" || letraOriginal === "@" || letraOriginal === "#" || letraOriginal === "$" || letraOriginal === "%" || letraOriginal === "&" || letraOriginal === "*" || letraOriginal === "(" || letraOriginal === ")") {
+//             letraNova = caracteresEspeciais;
+//         } else {
+//             letraNova = stringCorrompida[i];
+//         }
+//         resposta += letraNova
+//     }
+//     console.log(resposta);
+    
+// }
+
+
+// 08 - Empréstimo
+
+// function solucao(ganhos, serasaScore, mesesParaPagar) {
+//   //seu código aqui
+//     let juros = 0;
+    
+//     if (serasaScore < 300) {
+//         juros = 3;
+//         } else if (serasaScore < 700) {
+//             juros = 9;
+//         } else {
+//             juros = 15;
+//         }
+    
+//     const parcela = ((ganhos * 3) + (ganhos * (juros / 100))) / mesesParaPagar;
+    
+//     console.log(parcela);
+        
+// }
+
+
+// 09 - Forca
+
+// function solucao(palpite, palavra) {
+// 	// seu código aqui
+//     let acertos =0;
+    
+//     for (i = 0; i < palavra.length; i++) {
+//         if (palpite === palavra[i]) {
+//             acertos++
+//         }
+//     }
+//     console.log(acertos);    
+// }
+
+
+// 10 - Coincidência
+
+// function solucao(jogadas) {
+//   //seu código aqui
+//    let escolheuUm = 0;
+//    let escolheuZero = 0; 
+
+    
+//    for (i = 0; i < jogadas.length; i++) {
+//        const jogada = jogadas[i]
+              
+//        if (jogada === 1) {
+//             escolheuUm++;
+//         } else {
+//             escolheuZero++;
+//         }
+//     }
+    
+//     if (escolheuUm === escolheuZero) {
+//         console.log(true);
+//     } else {
+//         console.log(false);
+//     }
+        
+// }
+
+
